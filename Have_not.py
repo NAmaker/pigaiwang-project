6ac0cef592f389a1e93ceb61c3b028ad20dc5f5d
NAmaker <69228252+NAmaker@users.noreply.github.com>
2023-02-09 20:49:50 +0800

    Create Have_not.py

---
+++ Have_not.py
@@ -0,0 +1,31 @@
+
+from tkinter import *
+
+
+class Have_not():
+    x = None
+
+    def __init__(self):
+
+        window = Tk()
+        f = IntVar()
+        f.set(0)
+        self.window = window
+        self.window.title("是已经完成还是没完成？")
+        self.window.geometry("350x200")
+        rad1 = Radiobutton(self.window, text="完成", var=f, value=1, width=10, command=self.clicked1)
+        rad2 = Radiobutton(self.window, text="未完成", var=f, value=2, width=10, command=self.clicked2)
+        rad1.grid(column=0, row=0)
+        rad2.grid(column=1, row=0)
+        self.rad1 = rad1
+        self.rad2 = rad2
+        self.window.mainloop()
+
+    def clicked1(self, ):
+        self.x = 1
+
+    def clicked2(self, ):
+        self.x = 2
+
+have1_or_not2 = Have_not()
+have1_or_not2 = have1_or_not2.x
